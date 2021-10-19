f088604f60d37210bb04f9ca296f9936643e96f6
roncsak <809675+roncsak@users.noreply.github.com>
2021-10-19 15:17:00 +0200

    improve http statuses

--- getProduct/index.js
+++ getProduct/index.js
@@ -2,12 +2,21 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const productId = (req.query.productId || (req.body && req.body.productId));
-    const responseMessage = productId
-        ? `The product name for your product id ${productId} is Starfruit Explosion`
-        : "This HTTP triggered function executed successfully. Pass a productId in the query string or in the request body for a personalized response.";
+
+    let responseMessage = "This HTTP triggered function executed successfully. Pass a productId in the query string or in the request body for a personalized response.";
+    let status = 400;
+
+    if (productId) {
+        responseMessage = `The product name for your product id ${productId} is Starfruit Explosion`;
+        status = 200;
+    }
+    // const responseMessage = productId
+    //     ? `The product name for your product id ${productId} is Starfruit Explosion`
+    //     : "This HTTP triggered function executed successfully. Pass a productId in the query string or in the request body for a personalized response.";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
+        status: status,
         body: responseMessage
     };
 }
